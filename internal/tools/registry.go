55c4f0089d9fa6cf892204b88034480ea472fefd
RafaelZelak <rzf1503@gmail.com>
2026-01-08 09:42:38 -0300

    fix(.gitignore) - remove tools/

---
+++ internal/tools/registry.go
@@ -0,0 +1,61 @@
+package tools
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Registry struct {
+	tools []ToolConfig
+}
+
+func NewRegistry(path string) (*Registry, error) {
+	if path == "" {
+		return &Registry{tools: []ToolConfig{}}, nil
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	for i := range cfg.Tools {
+		if strings.HasPrefix(cfg.Tools[i].Conn, "ENV:") {
+			envKey := strings.TrimPrefix(cfg.Tools[i].Conn, "ENV:")
+			cfg.Tools[i].Conn = os.Getenv(envKey)
+		}
+	}
+
+	return &Registry{tools: cfg.Tools}, nil
+}
+
+func NewRegistryFromConfig(tools []ToolConfig) *Registry {
+	if tools == nil {
+		tools = []ToolConfig{}
+	}
+	return &Registry{tools: tools}
+}
+
+func (r *Registry) Get(name string) *ToolConfig {
+	for i := range r.tools {
+		if r.tools[i].Name == name {
+			return &r.tools[i]
+		}
+	}
+	return nil
+}
+
+func (r *Registry) List() []ToolConfig {
+	return r.tools
+}
+
+func (r *Registry) Add(tool ToolConfig) {
+	r.tools = append(r.tools, tool)
+}
